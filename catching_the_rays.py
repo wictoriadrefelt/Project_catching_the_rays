87ca951e65724da6368c527bf10e69be2012ec1e
Wictoria Drefelt <wictoria.soderlunddrefeldt@gmail.com>
2020-10-13 21:54:18 +0200

    Second commit

--- catching_the_rays.py
+++ catching_the_rays.py
@@ -1,9 +1,12 @@
+# Music contribution by Matthew Pablo
+
+
 import pygame
 import pygame.freetype
 import colors_scheme
 import random
 import os
-
+from os import path
 
 
 WIDTH = 900
@@ -13,15 +16,24 @@ MOVE = 7
 JUMP = - 20
 display = pygame.display.set_mode((640, 480))
 IMAGE = pygame.image.load(os.path.join('images', 'stylized-basic-cloud.png')).convert_alpha()
-#IMAGE = pygame.transform.scale(IMAGE, (130,130))
 SUN_IMAGE = pygame.image.load(os.path.join("images", "sun_shiny.png")).convert_alpha()
 SUN_IMAGE = pygame.transform.scale(SUN_IMAGE, (150, 150))
 C_IMAGE = pygame.image.load(os.path.join("images", "pumpkin.png")).convert_alpha()
 C_IMAGE = pygame.transform.scale(C_IMAGE, (70, 70))
+sound = path.join(path.dirname(__file__), "sound")
 
-# TODO Startcreen
+font_name = pygame.font.match_font("arial")
+# TODO Startscreen
 
-pygame.init()
+def happiness_score(screen, text, size, x, y):
+    font_name = pygame.font.match_font("arial")
+    font = pygame.font.Font(font_name, size)
+    text_box = font.render(text, True, colors_scheme.WHITE)
+    text_rect = text_box.get_rect()
+    text_rect.midtop = (x, y)
+    screen.blit(text_box, text_rect)
+
+
 class Cloud(pygame.sprite.Sprite):
     def __init__(self):    #MORE HERE?
         pygame.sprite.Sprite.__init__(self)
@@ -78,39 +90,40 @@ class Collector(pygame.sprite.Sprite):
             c = Cloud()
             all_sprites.add(c)
             clouds.add(c)
-            return c
+
        # if hits:
             #self.happiness <= 0:
 
 
 
-    def draw_happiness_bar(self):
-        if self.happiness > 70:
-            col = colors_scheme.GREEN
-        elif self.happiness > 40:
-            col = colors_scheme.YELLOW
-        else:
-            col = colors_scheme.RED
-        width = int(self.rect.width * self.happiness / 100)
-        self.happiness_bar = pygame.Rect(0, 0, width, 7)
-        if self.happiness < 100:
-            pygame.draw.rect(self.image, col, self.happiness_bar)
+    #def draw_happiness_bar(self):  #TODO, set this up
 
-def text_display(display, text, size, x, y):
-    def draw_text(surf, text, size, x, y):
-        pygame.font.init()
-        font_name = pygame.font.match_font("arial")
-        font = pygame.font.Font(font_name, size)
-        text_surface = font.render(text, True, colors_scheme.BLACK)
-        text_rect = text_surface.get_rect()
-        text_rect.centre = (x, y)
-        surf.blit(text_surface, text_rect)
+def start_screen():
+
+    startup = True
+
+    while startup:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        display.fill(colors_scheme.YELLOW)
+        largeText = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = text_objects("A bit Racey", largeText)
+        TextRect.center = ((WIDTH / 2), (HEIGHT / 2))
+        display.blit(TextSurf, TextRect)
+        pygame.display.update()
+        clock.tick(15)
 
 
 def main():
     pygame.init()
     pygame.font.init()
-    pygame.mixer.init()
+    pygame.mixer.init() #TODO, add sound and music
+    pygame.mixer.music.load(path.join(sound, 'Caketown1.mp3'))
+    pygame.mixer.music.set_volume(0.4)
     display = pygame.display.set_mode((HEIGHT, WIDTH))
     pygame.display.set_caption("Catch the rays")
     collector = Collector(HEIGHT//2, WIDTH//2, 40, 40, colors_scheme.YELLOW, 0, 0, display)
@@ -124,7 +137,6 @@ def main():
     all_sprites = pygame.sprite.Group()
     all_sprites.add(collector)
     clouds = pygame.sprite.Group()
-    #collector = pygame.sprite.Group()
     suns = pygame.sprite.Group()
     for i in range(7):
         c = Cloud()
@@ -135,7 +147,9 @@ def main():
         s = Sun()
         all_sprites.add(s)
         suns.add(s)
+    happiness = 50
 
+    pygame.mixer.music.play(loops=-1)
     running = True
     while running:
         clock.tick(FPS)
@@ -145,25 +159,30 @@ def main():
 
 
         all_sprites.update()
-        collector.collision(collector, clouds)
-        #for hit in hits:
-        #    c = Cloud()
-         #   all_sprites.add(c)
-          #  clouds.add(c)
+        #collector.collision(collector, clouds)
 
-        good_hits = pygame.sprite.spritecollide(collector, suns, True)
-        if good_hits:
+        collision = pygame.sprite.spritecollide(collector, clouds, True)
+        for hit in collision:
+            happiness -= 5
+            c = Cloud()
+            all_sprites.add(c)
+            clouds.add(c)
+
+        good_hit = pygame.sprite.spritecollide(collector, suns, True)
+        if good_hit:
             print("hit sun")
-        for hit in good_hits:
+        for hit in good_hit:
+            happiness += 5
             s = Sun()
             all_sprites.add(s)
             suns.add(s)
 
-        collector.draw_happiness_bar()
+
 
         display.fill(colors_scheme.LIGHT_BLUE)
-        text_display(display, str("HELLO"), WIDTH//2, 20, 20)
         all_sprites.draw(display)
+        #draw_happiness_bar()
+        happiness_score(display, str(happiness), 10, WIDTH // 2, 10)
         pygame.display.flip()
         clock.tick(100)
 
